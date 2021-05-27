67d1fca3376d9d6e013f0a0857564921dbe389f0
N4NO1 <nathan.rouse56734@gmail.com>
2021-05-27 02:02:14 +0100

    added patch request

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 const handleRow = require("./post")
 const request = require("request")
 const { getHeapCodeStatistics } = require("v8")
+const { resolveSoa } = require("dns")
 const source = process.argv[2]
 const accessToken = process.argv[3]
 const environment = process.argv[4] || "dev"
@@ -15,26 +16,43 @@ if (!(source && accessToken)) {
 channelRequest("84e87056-e624-41fc-bc01-37f28ef3931b")
 let bodyarray = []
 let listingIdArray=[]
+listingIdArray
 function channelRequest(channelId) {
     return new Promise((resolve, reject) =>{
         const options = {
-            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=5",
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=5&filter=%5Bstatus%5D!%3D%7B2%7D",
             method: "GET",
             headers: {authorization: "Bearer " + accessToken}
         }
     request(options, function handlePostResponse(error,response,body) {
+        console.log("GET",response.statusCode, response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + body.message)
             bodyarray = JSON.parse(body).data
             console.log(response.statusCode)
-            //console.log(i, bodyarray[index],bodyarray.length)
             bodyarray.forEach(function(element) {
                 listingIdArray.push(element.listingId)
             })
             console.log(listingIdArray)
+            listingIdArray.forEach(function(element){
+                    handleListing(element)
+            })
         return resolve()
         })
     })
 }
 
+function handleListing(listingId){
+    return new Promise((resolve, reject) =>{
+        const options = {
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/listings/" + listingId,
+            method: "PATCH",
+            headers: {authorization: "Bearer " + accessToken},
+        }
+        request(options, function handlePostResponse(error,responseP,bodyP) {
+            console.log("PATCH",listingId, responseP.statusCode, responseP.statusCode === 202 ? "SUCCESS" : "ERROR -- " + bodyP.message)
+        })
+        return resolve()
+    })
+}
     
 //var length = 0
 
