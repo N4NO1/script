75cae56b6c5b31e0639f93b9d0463d799ac24f12
N4NO1 <nathan.rouse56734@gmail.com>
2021-06-24 16:44:32 +0100

    created a script to bulk create item types

---
+++ Stokly-Scripts/createItemTypes.js
@@ -0,0 +1,43 @@
+var request = require("request")
+const fs = require("fs")
+const csv = require("csv-parser")
+const accessToken= process.argv[2]
+const environment = process.argv[3] || "dev"
+const timeDelay = process.argv[4]
+
+
+
+const stream = fs.createReadStream("itemtypes.csv")  //stream read checks ignore.csv for sale orders to ignore
+    .pipe(csv({
+        mapHeaders: ({ header, index }) => header.trim()
+    }))
+    .on("data", (data) => {
+        stream.pause()
+        const {name}=data
+        handleItemType(name).then(() => { setTimeout(() =>{stream.resume()},timeDelay) })
+    })
+    .on("end", () => { 
+        console.log("Done")
+    })
+
+    async function handleItemType(typeName){
+        const options = {
+            url:"https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/item-types",
+            method:"POST",
+            headers: {authorization: "Bearer " + accessToken},
+            json: true,
+            body:{name:typeName}
+        }
+        const response = await makeRequest(options)
+        console.log("POST","--", response.response.statusCode, response.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + response.body.message)
+    }
+
+    function makeRequest(options){
+        return new Promise((resolve, reject) =>{
+            request(options, (error, response, body) => {
+                if (error) {return reject(error)}
+                if (response.statusCode < 200 && response.statuscode > 299) {return reject(new error(body))}
+                return resolve({body, response})
+            })
+        })
+    }
