fe536046112a646b77631cd166b64b11e38ac857
N4NO1 <nathan.rouse56734@gmail.com>
2021-06-24 16:45:03 +0100

    started on account balances

---
+++ Stokly-Scripts/accountBalance.js
@@ -0,0 +1,9 @@
+var request = require("request")
+const accessToken= process.argv[2]
+const environment = process.argv[3] || "dev"
+const timeDelay = process.argv[4] || 0
+const channelId = process.argv[5]
+
+const customerOptions = {
+    
+}
