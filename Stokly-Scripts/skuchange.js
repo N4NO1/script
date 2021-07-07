6f3b722efa15416b51948cce837006119502aff6
Nathan Rouse <nathan.rouse56734@gmail.com>
2021-07-07 13:24:22 +0100

    finished and tested

--- Stokly-Scripts/skuchange.js
+++ Stokly-Scripts/skuchange.js
@@ -3,8 +3,25 @@ const fs = require("fs")
 const csv = require("csv-parser")
 const accessToken= process.argv[2]
 const environment = process.argv[3]
-const timeDelay = process.argv[4]
+const searchMethod = process.argv[4]
+const timeDelay = process.argv[5] || 100
 
+if(!(accessToken && environment)){
+    console.log("invalid parameters")
+    console.log("node skuchange.js accesstoken [environment (prod or dev)] [searchmethod (exact or contain)] [timedelay (default 100ms)]")
+    process.exit(1)
+}
+if(!(environment === "prod" || environment === "dev"))
+{
+    console.log("invalid enviroment")
+    console.log("accepted values: prod, dev")
+    process.exit(1)
+}
+if(!(searchMethod === "exact" || searchMethod === "contain")){
+    console.log("invalid searchMethod")
+    console.log("accepted values: exact, contain")
+    process.exit(1)
+}
 
 const stream = fs.createReadStream("Input CSVs\\skuinput.csv")  //stream read checks ignore.csv for sale orders to ignore
     .pipe(csv({
@@ -20,37 +37,48 @@ const stream = fs.createReadStream("Input CSVs\\skuinput.csv")  //stream read ch
     })
 async function handleSku(searchSku, newSku){
     const itemGetOptions={
-        url: "https://api."+(environment === "prod"?"":"dev.")+"stok.ly/v0/items?filter={sku}==["+searchSku+"]",
+        url: "https://api."+(environment === "prod"?"":"dev.")+"stok.ly/v0/items?filter={sku}"+(searchMethod === "exact"?"==":"::")+"["+searchSku+"]",
         method: "GET",
         headers: {authorization: "Bearer " + accessToken},
         json: true,
     }
     const getResponse = await makeRequest(itemGetOptions)
-    console.log(itemGetOptions.url)
     console.log("GET",itemGetOptions.url, "--",getResponse.response.statusCode, getResponse.response.statusCode === 200 ? "SUCCESS" : "ERROR -- "+ getResponse.body.message)
-    console.log(getResponse.body.data[0].itemId)
     if(getResponse.response.statusCode === 200 && getResponse.body.metadata.count !== 0){
-        if(getResponse.body.data[0].sku === searchSku){
+        var i = 1
+        var found = false
+        //checks if the correct sku is returned, allows the use of contains in the search above
+        if(searchMethod === "exact"){found = true}
+        do{
+        if(getResponse.body.data[i-1].sku === searchSku){
+            console.log("found SKU -",getResponse.body.data[i-1].sku)
             var itemId = getResponse.body.data[0].itemId
-            patchItem()
+            const patchBody = {sku:newSku}
+            patchItem(itemId,patchBody)
+            found = true
         }
-        
-    }
-}w
+        else if (i>= getResponse.body.metadata.count) {
+            console.log("Sku not found in response, skipping value")
+        }
+        else {
+            console.log("sku not found at data index", i-1)
+        }
+        i++
+    } while (i<= getResponse.body.metadata.count&& found === false)
+    } else {console.log(getResponse.response.statusCode === 200?"sku not found, skipping value:":"GET error skipping value")}
+}
 
-async function patchItem(itemId, newSku){
+async function patchItem(itemId,body){
     const itemPatchOptions={
         url:"https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/items/"+itemId,
         method:"PATCH",
         headers:{authorization: "Bearer " + accessToken},
-        json:true,
-        body:{
-                sku:newSku
-        }
+        json:true, 
+        body:body
     }
     //options end here
     const patchResponse = await makeRequest(itemPatchOptions)
-    console.log("PATCH","--", patchResponse.response.statusCode, patchResponse.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + patchResponse.body.message)
+    console.log("PATCH",itemPatchOptions.url,"new sku:"+body.sku,"--", patchResponse.response.statusCode, patchResponse.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + patchResponse.body.message)
 }
 
 function makeRequest(options){
