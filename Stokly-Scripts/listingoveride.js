86621d022a2132faf62713ff70bcdbdabae04e9d
Nathan Rouse <nathan.rouse56734@gmail.com>
2021-07-12 15:14:35 +0100

    added targetability to listing properties

--- Stokly-Scripts/listingoveride.js
+++ Stokly-Scripts/listingoveride.js
@@ -13,30 +13,41 @@ if (!(channelId && accessToken)) {
     process.exit(1)
 }
 handleChannel(channelId)
-//handles channel by Id - allows future use of channel list in csv
+
 async function handleChannel(channelId){
-    //sets options for Get channel request
+
     const getTotalOptions={
-            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=1&filter=%5Bstatus%5D!%3D%7B2%7D",
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=1&filter=(%5BdataSynchronised%5D%3A%3A%7B0%7D)%26%26(%5Bstatus%5D!%3D%7B2%7D)",
             method: "GET",
             headers: {authorization: "Bearer " + accessToken}
     }
-    //console.log(channelOptions.url)
+
     const totalResponse = await makeRequest(getTotalOptions)
     const totalData = JSON.parse(totalResponse.body)
     console.log("Listing Total:", totalData.metadata.count)
     for (var i=0;i+1 <= Math.ceil(totalData.metadata.count/100) ; i++){
         const channelOptions={
-            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=100&filter=%5Bstatus%5D!%3D%7B2%7D&page="+i,
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/channels/" + channelId +"/listings?size=100&filter=(%5BdataSynchronised%5D%3A%3A%7B0%7D)%26%26(%5Bstatus%5D!%3D%7B2%7D)&page="+i,
             method: "GET",
             headers: {authorization: "Bearer " + accessToken}
         }
     const channelResponse = await makeRequest(channelOptions)
+
     console.log("GET",(i*100)+1, "to",(i+1)*100 > totalData.metadata.count ? totalData.metadata.count : (i+1)*100,"of",totalData.metadata.count ,"--", channelResponse.response.statusCode, channelResponse.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + channelResponse.body.message)
+    
     const channelData = JSON.parse(channelResponse.body)
     console.log("removing overide of",channelData.data.length, "listings")
     for (const listing of channelData.data){
-        const listingOptionsConst = listingOptions(listing.listingId)
+        const getOptions={
+            url: "https://api.stok.ly/v0/listings/"+listing.listingId,
+            method: "GET",
+            headers: {authorization: "Bearer " + accessToken},
+            json: true,
+        }
+
+        getListingData = await makeRequest(getOptions)
+        console.log("GET",getOptions.url,getListingData.response.statusCode, getListingData.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + getListingData.body.message || "No Body")
+       const listingOptionsConst = listingOptions(listing.listingId, false,getListingData.body.data)
         listingResponse = await makeRequest(listingOptionsConst)
         console.log("PATCH",listingOptionsConst.url,listingResponse.response.statusCode, listingResponse.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + listingResponse.body.message || "No Body")
     }
@@ -52,13 +63,15 @@ function makeRequest(options){
         })
     })
 }
-function listingOptions(listingId) {
+function listingOptions(listingId, overideAll =Boolean, listingBody) {
     const options ={
     url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/listings/" + listingId,
     method: "PATCH",
     headers: {authorization: "Bearer " + accessToken},
     json: true,
-    body: {
+}
+if (overideAll === true) {
+    options.body ={
         data:{
         ageRestriction: null,
         allowNegativeInventory: null,
@@ -78,6 +91,13 @@ function listingOptions(listingId) {
         weight: null,
         }
     }
+} else {
+    listingBody.sku = null
+    listingBody.salePrice = listingBody.data.salePrice
+    listingBody.description = listingBody.data.description ?? null
+    listingBody.attributes = listingBody.data.attributes ?? null
+    delete listingBody.data
+    options.body = {data:listingBody}
 }
 return options
 }
