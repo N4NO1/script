a8acbd400d077c1ddcfce835a81c4024774254dd
Nathan Rouse <nathan.rouse56734@gmail.com>
2022-02-17 16:15:09 +0000

    Rewritten to stop persisting values

--- Stokly-Scripts/addAttributes.js
+++ Stokly-Scripts/addAttributes.js
@@ -1,88 +1,155 @@
-var request = require("request")
-const fs = require("fs")
-const csv = require("csv-parser")
-const { Console } = require("console")
+////////////////////////////////////////////////////////////////////////////////////////
+//variables in this section are set at runtime or are required modules for this script//
+////////////////////////////////////////////////////////////////////////////////////////
+var request = require("request")                                                      //
+const fs = require("fs")                                                              //
+const csv = require("csv-parser")                                                     //
+const { Console } = require("console")                                                //
+var current = new Date()                                                              //
+////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 const accessToken= process.argv[2]
 const environment = process.argv[3]
-const attributeIds = [
-    "eaa6afc1-ec9e-486b-a113-bba6522dce05",
-    "028c84ba-4590-416f-a4e6-796cec5116bf",
-    "c2260588-5752-4ec5-947b-4f5a06c6a0e6",
-    "c344246b-55f7-4395-a40e-a40409bd8889",
-    "9c12c1d5-5c97-482d-b972-a644b9ff14f7",
-    "09c336f9-693b-4ae0-a047-16c7fb7b962c",
-    "4067f646-a843-4959-9315-4c7ca2e8b679",
-    "1e59b1e0-afef-4819-ab83-565c5340a0dc",
-    "fc0066cf-830a-4f23-83d9-4bfd263ed665",
-    "4836592e-c187-4db3-959c-e35dd72b789a",
-    "c4f994b1-7e46-405d-aebf-7f53e8157e8e",
-    "d8be2159-fbef-4188-89a3-d9c554976429",
-    "70c16ad2-8d17-4229-a970-5d496ef6fc13",
-    "8769454d-587f-4564-8950-2cb6b90c8cbe"
+
+
+//attributes to add to item types
+const addAttributeIds = [
+    "2c141905-2ac9-46f8-b42b-dd964750c7ff",
+    "6a2d7257-5e73-451f-80e2-3533ba1117b9"
 ]
+var skippedPages = []
+
+getItemPage()
 
 
-typeControl()
+async function getItemPage() {
+    var pageNumber = 0
+//get 100 item types a page at a time, until less that 100 are returned
+    do {
 
-async function typeControl() {
-
-    const getAllTypesOptions = {
-        url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/item-types?size=1000&filter=[status]!={1}",
-        method: "GET",
-        headers: {authorization: "Bearer " + accessToken}
-    }
-
-
-    const allTypes = await makeRequest(getAllTypesOptions)
-    console.log("GET","total item types:",JSON.parse(allTypes.body).metadata.count ,"--", allTypes.response.statusCode, allTypes.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + allTypes.body.message)
-    const data = JSON.parse(allTypes.body).data
-
-
-    for (const itemType of data) {
-        await appendAttributes(itemType.itemTypeId)
-    }
-
-}
-
-async function appendAttributes(id) {
-    const appendOptions = {
-        url:"https://api."+(environment === "prod" ? "" : "dev." )+"stok.ly/v0/item-types/"+id,
-        method:"PATCH",
-        headers: {authorization: "Bearer " + accessToken},
-        json:true,
-        body:{
-            itemAttributeIds: await attributeIdArray(id)
+        //set options to get page x from stok.ly
+        const getPageOptions = {
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/item-types?size=100&filter=[status]!={1}&page=" + pageNumber,
+            method: "GET",
+            headers: {authorization: "Bearer " + accessToken},
+            json: true
         }
-    }
 
-    const postRequest = await makeRequest(appendOptions)
-    console.log("PATCH",id ,"--", postRequest.response.statusCode, postRequest.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + postRequest.body.message)
+        //send request wait for the response, then store it in a variable. Set the time the response was received
+        const itemTypePageFullResponse = await makeRequest(getPageOptions)
+        current = new Date()
+
+        //check the status of the response, was it successful? code=200
+        console.log(current.toISOString(),"|","GET Item Types",itemTypePageFullResponse.response.statusCode, itemTypePageFullResponse.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + itemTypePageFullResponse.body.message )
+
+        //if the request was successful, get the page length and enter the rest of the code, else skip to next page
+        if (itemTypePageFullResponse.response.statusCode == 200) {
+
+            // separate the data array from the response data so the variable is smaller
+            itemTypePageBodyData = itemTypePageFullResponse.body.data
+
+            //set the page length for the do/while
+            pageLength = itemTypePageBodyData.length
+
+            //for each item type run this loop
+            for(itemType of itemTypePageBodyData) {
+
+                //handle item type
+               const assignedAttributes = await handleIndividualType(itemType.itemTypeId)
+
+               //if null is returned write a error message, and continue to next iteration
+                if (assignedAttributes == null)  {
+                    console.error(">>>skipping item type due to request error:" + itemType.name +"<<<")
+                    continue
+                }
+                var outputArray = []
+                console.log("adding new attributes")
+                for (addAttributeId of addAttributeIds){
+                    outputArray.push(addAttributeId)
+                }
+                console.log("adding existing attributes")
+                for (assignedAttributeId of assignedAttributes) {
+                    outputArray.push(assignedAttributeId)
+                }
+
+                console.log("Existing attribute count :" + assignedAttributes.length, "Added attributes count :" + addAttributeIds, "Output attributes Count :" + outputArray.length)
+
+                if (outputArray.length == (assignedAttributes.length + addAttributeIds.length)) {
+                    Console.log("Counts Match, patch item")
+                    await patchType(id, outputArray)
+                } else {
+                    console.error(">>>counts do not match!<<<")
+                }
+            }
+
+        } else {
+            //if page request failed, push page number to array for notice at end
+            skippedPages.push(pageNumber)
+            console.error(">>>Skipped page #" + pageNumber + "<<<")
+        }
+
+
+        pageNumber++
+    } while (pageLength == 100)
 }
 
-
-async function attributeIdArray(id) {
-    const getOneOptions = {
-        url:"https://api."+(environment === "prod" ? "" : "dev." )+"stok.ly/v0/item-types/"+id+"/attributes",
-        method:"GET",
+async function handleIndividualType(typeId) {
+    
+    //Set options for getting an item type
+    const getTypeOptions = {
+        url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/item-types/" + typeId +"/attributes?size=1000&filter=[status]!={1}",
+        method: "GET",
         headers: {authorization: "Bearer " + accessToken},
+        json: true
     }
 
-    const getOneResponse = await makeRequest(getOneOptions)
-    console.log("GET" ,"--", getOneResponse.response.statusCode, getOneResponse.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + getOneResponse.body.message)
-    return await createAttributeArray(attributeIds, JSON.parse(getOneResponse.response.body).data)
+    const itemTypeFullResponse = await makeRequest(getTypeOptions)
+    current = new Date()
+        //check the status of the response, was it successful? code=200
+        console.log(current.toISOString(),"|","GET Item Types",itemTypeFullResponse.response.statusCode, itemTypeFullResponse.response.statusCode === 200 ? "SUCCESS" : "ERROR -- " + itemTypeFullResponse.body.message )
+
+        //check if the request was successful
+        if(itemTypeFullResponse.response.statusCode == 200) {
+            //create an empty array for existing IDs to be pushed to
+            var existingAttributes = []
+
+            for (attribute of itemTypeFullResponse.body.data) {
+
+                //push existing attributes to the array
+                existingAttributes.push(attribute.itemAttributeId)
+            }
+
+            //return the array as result
+            return existingAttributes
+
+        }else {
+            //if request failed, return null, skip item type
+            return null
+        }
+
 }
 
-
-async function createAttributeArray(addIds, currentData) {
-    var attributeArray = addIds
-    for(var attributeData of currentData) {
-        await attributeArray.push(attributeData.itemAttributeId)
+async function patchType(id, attributeArray){
+    
+    const patchOptions = {
+            url: "https://api." + (environment === "prod" ? "" : "dev." ) + "stok.ly/v0/item-types/"+id,
+            method: "PATCH",
+            headers: {authorization: "Bearer " + accessToken},
+            json: true,
+            body: {
+                itemAttributeIds: attributeArray
+            }
     }
-    return attributeArray
-}
 
+    const patchResponse = await makeRequest(patchOptions)
+    current = new Date()
+    
+    console.log(current.toISOString(),"|","Patch Item Type:"+id,itemTypeFullResponse.response.statusCode, itemTypeFullResponse.response.statusCode === 202 ? "SUCCESS" : "ERROR -- " + itemTypeFullResponse.body.message )
+
+}
 
 function makeRequest(options, retryAttempt = 0){
     return new Promise((resolve, reject) =>{
